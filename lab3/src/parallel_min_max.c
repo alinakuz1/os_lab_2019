d87d18429d203b0fade95e49bd304906fa4574a5
alinakuz1 <alinakuzminaa@gmail.com>
2019-05-06 11:34:39 +0000

    lab 3 and 4_1

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <unistd.h>
 
+#include<signal.h>
+
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -15,10 +17,15 @@
 #include "find_min_max.h"
 #include "utils.h"
 
+int kill_func(){
+     exit(2);
+}
+
 int main(int argc, char **argv) {
   int seed = -1;
   int array_size = -1;
   int pnum = -1;
+  int timeout=-1;
   bool with_files = false;
 
   while (true) {
@@ -28,8 +35,9 @@ int main(int argc, char **argv) {
                                       {"array_size", required_argument, 0, 0},
                                       {"pnum", required_argument, 0, 0},
                                       {"by_files", no_argument, 0, 'f'},
+                                      {"timeout", required_argument, 0, 't'},
                                       {0, 0, 0, 0}};
-
+//optional_argument
     int option_index = 0;
     int c = getopt_long(argc, argv, "f", options, &option_index);
 
@@ -42,19 +50,38 @@ int main(int argc, char **argv) {
             seed = atoi(optarg);
             // your code here
             // error handling
+            if(seed<=0){
+                printf("seed must be a positive num\n ");
+                return 1;
+            }
             break;
           case 1:
             array_size = atoi(optarg);
             // your code here
             // error handling
+            if(array_size<=0){
+                printf("array_size must be a positive num\n");
+                return 1;
+            }
             break;
           case 2:
             pnum = atoi(optarg);
             // your code here
             // error handling
+            if(pnum<=0){
+                printf("pnum must be a positive num\n");
+                return 1;
+            }
             break;
           case 3:
             with_files = true;
+            break;
+          case 4:
+            timeout = atoi(optarg);
+            if(timeout<=0){
+            printf("timeout must be a positive num\n");
+            return 1;
+            }
             break;
 
           defalut:
@@ -63,6 +90,13 @@ int main(int argc, char **argv) {
         break;
       case 'f':
         with_files = true;
+        break;
+      case 't':
+        timeout = atoi(optarg);
+        if(timeout<=0){
+            printf("timeout must be a positive num\n");
+            return 1;
+        }
         break;
 
       case '?':
@@ -79,7 +113,7 @@ int main(int argc, char **argv) {
   }
 
   if (seed == -1 || array_size == -1 || pnum == -1) {
-    printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" \n",
+    printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" --timeout (num)\n",
            argv[0]);
     return 1;
   }
@@ -87,26 +121,52 @@ int main(int argc, char **argv) {
   int *array = malloc(sizeof(int) * array_size);
   GenerateArray(array, array_size, seed);
   int active_child_processes = 0;
-
+  //for(int i = 0; i < array_size; i++)
+    //array[i]%=21, printf("%i ", array[i]);//??
+    //printf("\n");
+    
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
 
+    int part = array_size/pnum;
+    int pipefd[2];
+    
+    pipe(pipefd);
+    
+    pid_t currentPID;
+    
+    pid_t* pids = malloc(sizeof(pid_t) * pnum);
+    
   for (int i = 0; i < pnum; i++) {
     pid_t child_pid = fork();
+    currentPID = child_pid;
     if (child_pid >= 0) {
       // successful fork
-      active_child_processes += 1;
+      active_child_processes++;
       if (child_pid == 0) {
-        // child process
+         signal(SIGKILL, kill_func);
+         struct MinMax mm;
+         if(i*part < array_size)
+             mm = GetMinMax(array + i*part, 
+                            0,
+                            part);
+         else
+            mm = GetMinMax(array + array_size - part, 0, part);
 
-        // parallel somehow
-
+            //printf(COLOR _BOLD _RED "\n\tPARENT: %d, this->PID: %d, CHILD: %d || min: %i, max: %i\n" COLOR _NC, getppid(), getpid(), currentPID, mm.min, mm.max);
         if (with_files) {
-          // use files here
+          FILE* fp = fopen("processOut.txt", "a");
+          fwrite(&mm, sizeof(struct MinMax), 1, fp);
+          fclose(fp);
         } else {
           // use pipe here
+          write(pipefd[1],&mm,sizeof(struct MinMax));
+          
         }
         return 0;
+      }
+      else{
+          pids[i] = child_pid;
       }
 
     } else {
@@ -115,28 +175,42 @@ int main(int argc, char **argv) {
     }
   }
 
-  while (active_child_processes > 0) {
-    // your code here
 
-    active_child_processes -= 1;
-  }
+if(timeout>0){
+    sleep(timeout);
+    for(int k = 0; k<pnum; k++)
+        kill(pids[k], SIGKILL);
+    printf("timeout!\n");
+}
+    while (active_child_processes > 0) {
+        wait(NULL);
+        active_child_processes--;
+    }
 
   struct MinMax min_max;
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
 
-  for (int i = 0; i < pnum; i++) {
-    int min = INT_MAX;
-    int max = INT_MIN;
 
+  for (int i = 0; i < pnum; i++) {
+    struct MinMax mm;
+    
     if (with_files) {
       // read from files
+        FILE* fp = fopen("processOut.txt", "rb");
+        //printf(COLOR _BOLD _BLUE "\nBYTE FILE POS: %i, FILE POINTER: %p" COLOR _NC, i*sizeof(struct MinMax),*fp);
+        fseek(fp, i*sizeof(struct MinMax), SEEK_SET);
+        fread(&mm, sizeof(struct MinMax), 1, fp);
+        //printf(COLOR _BOLD _GREEN "\n[FROM FILE: min:%i  max:%i]" COLOR _NC,mm.min, mm.max);
+        fclose(fp);
     } else {
       // read from pipes
+      read(pipefd[0], &mm, sizeof(struct MinMax));
+      //printf(COLOR _BOLD _GREEN "\n[FROM PIPE: min:%i  max:%i]" COLOR _NC,mm.min, mm.max);
     }
 
-    if (min < min_max.min) min_max.min = min;
-    if (max > min_max.max) min_max.max = max;
+    if (mm.min < min_max.min) min_max.min = mm.min;
+    if (mm.max > min_max.max) min_max.max = mm.max;
   }
 
   struct timeval finish_time;
@@ -146,10 +220,13 @@ int main(int argc, char **argv) {
   elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
 
   free(array);
+  free(pids);
 
-  printf("Min: %d\n", min_max.min);
+  printf("\nMin: %d\n", min_max.min);
   printf("Max: %d\n", min_max.max);
   printf("Elapsed time: %fms\n", elapsed_time);
   fflush(NULL);
+  
+    remove("processOut.txt");
   return 0;
 }
